4a984c7669d55f37592f6c9411cff74cd9639031
sandeepn97 <snyoupane1s@gmail.com>
2022-12-01 21:51:35 -0600

    commit added

--- src/inheritance/InhritanceConcept.java
+++ src/inheritance/InhritanceConcept.java
@@ -1,5 +1,11 @@
 package inheritance;
 
 public class InhritanceConcept {
-
+	// A process of converting one data type to another is known as Typecasting
+	// UPcasting and DownCasting is the type of object typecasting
+	// the boject can also be typecasted like a datatypes
+	// parent and child objects are tow types of object
+	//new line 
+	
 }
+        
