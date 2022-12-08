8924cd29eefb0e80a04e69d159ba3bf09538d2dd
sandeepn97 <snyoupane1s@gmail.com>
2022-12-07 19:25:00 -0600

    final keyword

---
+++ src/finalKeyword/Car1.java
@@ -0,0 +1,10 @@
+package finalKeyword;
+//2.Create a class vehicle having final method as speed().
+//Create a another inheriting the class Vehicle and method speed().
+//In the main class access that speed().
+public class Car1 extends Car{
+		
+	public void horn() {
+		System.out.println("How to horn dafd ");
+	}
+}
